72c7dd9db827814d3c095412f00de11e98f7d6fa
Donovan <donovanle18@gmail.com>
2020-12-26 14:43:00 -0600

    fixed up mobile/desktop

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,83 +1,115 @@
-import styled from 'styled-components';
-import NavBar from './NavBar/NavBar';
-import backImgMobile from './images/prple-controller.jpg';
-import backImgDesktop from './images/controller-desktop.jpg';
+import styled from "styled-components";
+import NavBar from "./NavBar/NavBar";
+import backImgMobile from "./images/prple-controller.jpg";
+import backImgDesktop from "./images/controller-desktop.jpg";
 
 const Home = () => {
-  return (
-    <StyledMain>
-      <div className='landing-page'>
-        <div>
-          <nav>
-            <NavBar />
-          </nav>
-        </div>
-        <div className='main-content'>
-          <div className='title-motto'>
-            <h1>Gaming Social Network</h1>
-            <span>Connecting Gamers since 2077</span>
-          </div>
-          <div className='intro-video'>
-            <iframe
-              title='mock video'
-              width='420'
-              height='320'
-              src='https://www.youtube.com/embed/eW7Twd85m2g&ab_channel=StarWars'
-            ></iframe>
-          </div>
-        </div>
-      </div>
-    </StyledMain>
-  );
+    return (
+        <StyledMain>
+            <div className='landing-page'>
+                <div>
+                    <nav>
+                        <NavBar />
+                    </nav>
+                </div>
+                <div className='main-content'>
+                    <div className='title-motto'>
+                        <h1>Gaming Social Network</h1>
+                        <span>Connecting Gamers since 2077</span>
+                        <button>Sign Up</button>
+                    </div>
+                    <div className='intro-video'>
+                        <iframe
+                            title='mock video'
+                            src='https://www.youtube.com/embed/eW7Twd85m2g'
+                        ></iframe>
+                    </div>
+                </div>
+            </div>
+        </StyledMain>
+    );
 };
 
 const StyledMain = styled.main`
-  background: url(${backImgMobile}) no-repeat;
-  background-size: 100% 100%;
-  height: 100vh;
-
-  .title-motto {
-    padding-top: 14rem;
-    text-align: center;
-    margin: auto;
-  }
-
-  iframe {
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #14ffec;
-    padding-bottom: 0.8rem;
-  }
-
-  span {
-    color: white;
-    font-size: 1.9rem;
-  }
-
-  @media all and (min-width: 700px) {
-    background: url(${backImgDesktop}) no-repeat;
-    background-size: 100% 100%;
-
-    .main-content {
-      margin-top: 30rem;
-      padding: 0.5rem;
-    }
+    background: url(${backImgMobile}) no-repeat;
+    background-size: 230% 110%;
+    background-position: -40rem -3rem;
+    height: 100vh;
 
     .title-motto {
-      width: 60%;
-      float: left;
+        padding-top: 14rem;
+        text-align: center;
+        margin: auto;
+    }
+
+    iframe {
+        margin: 0 auto;
     }
 
     h1 {
-      font-size: 4rem;
+        color: #14ffec;
+        padding-bottom: 0.8rem;
     }
 
     span {
-      font-size: 2rem;
+        color: white;
+        font-size: 1.9rem;
     }
-  }
+
+    @media (max-width: 700px) {
+        .title-motto button {
+            display: none;
+        }
+
+        iframe {
+            display: none;
+        }
+    }
+
+    @media all and (min-width: 700px) {
+        background: url(${backImgDesktop}) no-repeat;
+        background-size: 100% 100%;
+
+        .main-content {
+            margin-top: 30rem;
+            padding: 0.5rem;
+        }
+
+        .title-motto {
+            width: 50%;
+            margin: auto;
+            margin-left: 15rem;
+            text-align: left;
+            float: left;
+            display: flex;
+            flex-direction: column;
+        }
+
+        h1 {
+            font-size: 6rem;
+        }
+
+        span {
+            font-size: 3rem;
+        }
+
+        .title-motto button {
+            margin-top: 1rem;
+            width: 13rem;
+            padding: 0.9rem 1rem 0.9rem 1rem;
+            border: solid 1px black;
+            border-radius: 0.5rem;
+            background-color: #0d7377;
+            color: white;
+        }
+
+        iframe {
+            width: 52rem;
+            height: 37rem;
+            border: none;
+            box-shadow: 5px 5px 5px black;
+        }
+    }
 `;
 
 export default Home;
--- src/components/Home/NavBar/NavBar.js
+++ src/components/Home/NavBar/NavBar.js
@@ -1,25 +1,38 @@
-import { FaBars } from 'react-icons/fa';
-import styled from 'styled-components';
+import { FaBars } from "react-icons/fa";
+import styled from "styled-components";
 
 const NavBar = () => {
-  return (
-    <StyledNav>
-      <button className='mobile-nav-btn'>
-        <FaBars />
-      </button>
-
-      <div>
-        <div>
-          <button>Login</button>
-        </div>
-        <div>
-          <button>Sign Up</button>
-        </div>
-      </div>
-    </StyledNav>
-  );
+    return (
+        <StyledNav>
+            <button className='mobile-nav-btn'>
+                <FaBars />
+            </button>
+            <div className='desktop-nav-btns'>
+                <div>
+                    <button>Login</button>
+                </div>
+                <div>
+                    <button>Sign Up</button>
+                </div>
+            </div>
+        </StyledNav>
+    );
 };
 
-const StyledNav = styled.nav``;
+const StyledNav = styled.nav`
+    @media (max-width: 700px) {
+        .desktop-nav-btns button {
+            display: none;
+        }
+    }
+    @media all and (min-width: 700px) {
+        .mobile-nav-btn {
+            display: none;
+        }
+        .desktop-nav-btns {
+            display: flex;
+        }
+    }
+`;
 
 export default NavBar;
